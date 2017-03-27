9d889e003e3db7d58c1544cfb921b3b3def16ddf
Blake Ingram <blakeingram@gmail.com>
2017-03-27 20:56:18 +0800

    Create header and sidebar. Start basic styling for layout purposes.

--- js/script.js
+++ js/script.js
@@ -2,11 +2,23 @@
 Neighborhood Map project FEND
 */
 
+
+
+/* MODELS */
+// an array of default locations
+var defaultLocations = [];
+
+// an array of user input locations TODO: include user input later.
+var userLocations = [];
+
+
+
+
 // Initialize Google Map
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 48.7435276, lng: -122.4856877},
-    zoom: 12
+    zoom: 13
   });
 }
