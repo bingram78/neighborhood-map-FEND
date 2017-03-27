291320468e40e6f7dec579f7b43a5c2d5a0693d9
Blake Ingram <blakeingram@gmail.com>
2017-03-27 18:33:02 +0800

    Add Google Map full page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 /*
 Neighborhood Map project FEND
 */
+
+// Initialize Google Map
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 48.7435276, lng: -122.4856877},
+    zoom: 12
+  });
+}
