ddac5410daefc4b3d564b19f29448b1c7275b97a
Blake Ingram <blakeingram@gmail.com>
2017-04-05 09:57:28 +0800

    Remove bad code sections. Revert to basic map with list of locations in side bar. No markers, etc.

--- js/app.js
+++ js/app.js
@@ -12,10 +12,10 @@ var defaultLocations = [
 ];
 
 // not sure yet if these two are needed.
+
 var map;
 // var markers = [];
 var nightModeStyle = [];
-
 
 // Initializes google map on page
 function initMap() {
@@ -25,47 +25,11 @@ function initMap() {
     // style: nightModeStyle
   };
   var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-  var placeService = new google.maps.places.PlacesService(map);
-
-  var bindInfoWindow = function(marker, address) {
-    var infoWindow = new google.maps.InfoWindow( {
-      content: address
-    });
-    marker.addListener('click', function() {
-      infoWindow.open(marker.get('map'), marker);
-    });
-  };
-
-  var createMarker = function(loc, pos) {
-    var marker = new google.maps.Marker( {
-      map: map,
-      title: title,
-      position: pos,
-      animation: google.maps.Animation.Drop,
-      content: address
-    })
-    bindInfoWindow(marker, address);
-  };
-
-  for (var i = 0; i < defaultLocations.length; i++) {
-    var title = defaultLocations[i].name;
-    var address = '<strong>' + defaultLocations[i].address + '</strong>';
-    var places = defaultLocations[i].place;
-    placeService.getDetails({
-      placeId: places
-    }, function(place, status) {
-      if (status !== google.maps.places.PlacesServiceStatus.OK) {
-        console.log("this wrong");
-        }
-      else {
-        var pos = place.geometry.location;
-        createMarker(i, pos);
-      }
-    });
-  }
-
+  var placeServices = new google.maps.places.PlacesService(map);
 };
 
+
+// Function to make the items in the location array observable to be used in viewmodel.
 var ViewLocations = function(data) {
   this.name = ko.observable(data.name);
   this.address = ko.observable(data.address);
@@ -73,21 +37,16 @@ var ViewLocations = function(data) {
 };
 
 
-
-
-function viewModel() {
+// MV for interacting with html.
+function ViewModel() {
   var self = this;
-  // var name = ko.observable();
-  // self.name = "blake"
-  // self.view = function locationViewer(name) {
-  //   console.log('hi');
-  // };
   this.locationList = ko.observableArray([]);
   defaultLocations.forEach(function(i) {
     self.locationList.push( new ViewLocations(i) );
   });
 
+
 };
 
-
-ko.applyBindings(new viewModel());
+var vm = new ViewModel();
+ko.applyBindings(vm);
