e36ffde0ad722d824645564844c808615ee544ee
Blake Ingram <blakeingram@gmail.com>
2017-04-06 13:15:36 +0800

    Remove jQuery document request from viewmodel bindings apply. Add defer to app.js script loading.

--- js/app.js
+++ js/app.js
@@ -14,21 +14,103 @@ var defaultLocations = [
 var map;
 var service;
 var geocode;
+var marker;
 // var markers = [];
-var nightModeStyle = [];
+var nightModeStyle = [
+        {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+        {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+        {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+        {
+          featureType: 'administrative.locality',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'poi',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'geometry',
+          stylers: [{color: '#263c3f'}]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#6b9a76'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry',
+          stylers: [{color: '#38414e'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#212a37'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#9ca5b3'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry',
+          stylers: [{color: '#746855'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#1f2835'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#f3d19c'}]
+        },
+        {
+          featureType: 'transit',
+          elementType: 'geometry',
+          stylers: [{color: '#2f3948'}]
+        },
+        {
+          featureType: 'transit.station',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'geometry',
+          stylers: [{color: '#17263c'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#515c6d'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.stroke',
+          stylers: [{color: '#17263c'}]
+        }
+      ];
 
 // Initializes google map on page
 function initMap() {
   var mapOptions = {
     center: {lat: 48.7435276, lng: -122.4856877},
     zoom: 13,
-    // style: nightModeStyle
+    styles: nightModeStyle
   };
   map = new google.maps.Map(document.getElementById('map'), mapOptions);
   service = new google.maps.places.PlacesService(map);
   geocode = new google.maps.Geocoder();
 };
 
+// Displays error alert box that Maps could not load.
+// Called from google api script loading in HTML file.
 var googleError = function() {
   console.log("no map");
   window.alert("Maps cannot load. Please check yo' situation!;")
@@ -53,11 +135,11 @@ function ViewModel() {
   defaultLocations.forEach(function(i) {
     self.locationList.push( new ViewLocations(i) );
   });
+  
+
 
 
 };
 
-$(document).ready(function() {
-  var vm = new ViewModel();
-  ko.applyBindings(vm);
-});
+var vm = new ViewModel();
+ko.applyBindings(vm);
